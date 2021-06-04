f07c606d9c0d3268fc6d2c855aa25ca04c1105fc
Daniel Nakamashi <dnakamashi@gmail.com>
2021-06-04 15:02:28 +0200

    refactor: removed children form FormNode

---
+++ packages/core/src/entities/FormNode/FormNode.test.ts
@@ -0,0 +1,23 @@
+import { NodeType } from 'enums/NodeType'
+import { FormNode } from './FormNode'
+
+describe('FormNode', () => {
+  it('should convert to Json', () => {
+    const formNode = new FormNode('id')
+
+    expect(formNode.toJson()).toEqual({
+      id: 'id',
+      type: NodeType.FormNode,
+    })
+  })
+
+  it('should convert from Json', () => {
+    const formNode = FormNode.fromJson({
+      id: 'id',
+      type: NodeType.FormNode,
+    })
+
+    expect(formNode.id).toEqual('id')
+    expect(formNode.type).toEqual(NodeType.FormNode)
+  })
+})
